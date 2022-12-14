32d8b215d57534825efbb90c8f5cb48f76ad75ff
Someone-dQw4w9WgXcQ <120410318+Someone-dQw4w9WgXcQ@users.noreply.github.com>
2022-12-14 16:43:43 +0000

    prepare for optimisation

--- module/serializerList.lua
+++ module/serializerList.lua
@@ -2,7 +2,9 @@
 
 local serializers = {}
 local deserializers = {}
-local valueIds = {
+
+-- TODO: compress these (will break backwards compatibility)
+local typeToId = {
 	CFrame = "C",
 	Vector3 = "V",
 	Color3 = "C3",
@@ -13,52 +15,63 @@ local valueIds = {
 	Vector2int16 = "V2i",
 	Vector3int16 = "Vi"
 }
+local idToType = {
+	C = "CFrame",
+	V = "Vector3",
+	C3 = "Color3",
+	E = "EnumItem",
+	B = "BrickColor",
+	T = "TweenInfo",
+	V2 = "Vector2",
+	V2i = "Vector2int16",
+	Vi = "Vector3int16"
+}
 
-type serializedCFrame = {typeof(valueIds.CFrame) | number}
-type serializedColor3 = {typeof(valueIds.Color3) | number}
-type serializedEnumItem = {typeof(valueIds.EnumItem) | number}
-type serializedBrickColor = {typeof(valueIds.BrickColor) | string}
-type serializedTweenInfo = {typeof(valueIds.TweenInfo) | number | Enum.EasingStyle | Enum.EasingDirection | boolean}
-type serializedVector2 = {typeof(valueIds.Vector2) | number}
-type serializedVector3 = {typeof(valueIds.Vector3) | number}
-type serializedVector2int16 = {typeof(valueIds.Vector2int16) | number}
-type serializedVector3int16 = {typeof(valueIds.Vector3int16) | number}
+type serializedCFrame = {typeof(typeToId.CFrame) | number}
+type serializedColor3 = {typeof(typeToId.Color3) | number}
+type serializedEnumItem = {typeof(typeToId.EnumItem) | number}
+type serializedBrickColor = {typeof(typeToId.BrickColor) | string}
+type serializedTweenInfo = {typeof(typeToId.TweenInfo) | number | Enum.EasingStyle | Enum.EasingDirection | boolean}
+type serializedVector2 = {typeof(typeToId.Vector2) | number}
+type serializedVector3 = {typeof(typeToId.Vector3) | number}
+type serializedVector2int16 = {typeof(typeToId.Vector2int16) | number}
+type serializedVector3int16 = {typeof(typeToId.Vector3int16) | number}
 
 -- serializers
 function serializers.CFrame(cframe : CFrame): serializedCFrame
-	return {valueIds.CFrame, cframe:GetComponents()}
+	return {typeToId.CFrame, cframe:GetComponents()}
 end
 
 function serializers.Color3(value : Color3): serializedColor3
-	return {valueIds.Color3, value.R, value.G, value.B}
+	return {typeToId.Color3, value.R, value.G, value.B}
 end
 
 function serializers.EnumItem(value : EnumItem): serializedEnumItem
-	return {valueIds.EnumItem, tostring(value.EnumType), value.Name}
+	return {typeToId.EnumItem, tostring(value.EnumType), value.Name}
 end
 
 function serializers.BrickColor(value : BrickColor): serializedBrickColor
-	return {valueIds.BrickColor, value.Name}
+	return {typeToId.BrickColor, value.Name}
 end
 
 function serializers.TweenInfo(value : TweenInfo): serializedTweenInfo
-	return {valueIds.TweenInfo, value.Time, value.EasingStyle, value.EasingDirection, value.RepeatCount, value.Reverses, value.DelayTime}
+	return {typeToId.TweenInfo, value.Time, value.EasingStyle, value.EasingDirection, value.RepeatCount, value.Reverses, value.DelayTime}
 end
 
 function serializers.Vector2(value : Vector2): serializedVector2
-	return {valueIds.Vector2, value.X, value.Y}
+	return {typeToId.Vector2, value.X, value.Y}
 end
 
 function serializers.Vector2int16(value : Vector2int16): serializedVector2int16
-	return {valueIds.Vector2int16, value.X, value.Y}
+	return {typeToId.Vector2int16, value.X, value.Y}
 end
 
 function serializers.Vector3(vector3 : Vector3): serializedVector3
-	return {valueIds.Vector3, vector3.X, vector3.Y, vector3.Z} -- it has to be hard
+	return {typeToId.Vector3, vector3.X, vector3.Y, vector3.Z} -- it has to be hard
 end
 
 function serializers.Vector3int16(value : Vector3int16): serializedVector3int16
-	return {valueIds.Vector3int16, value.X, value.Y, value.Z}
+	return {typeToId.Vector3int16, value.X, value.Y, value.Z}
 end
 
 -- deserializers (Note these does NOT Check for the type of table's value, if you want it to, use init.lua)
@@ -101,5 +114,6 @@ end
 return table.freeze({
 	serializers = table.freeze(serializers),
 	deserializers = table.freeze(deserializers),
-	valueIds = table.freeze(valueIds)
+	typeToId = table.freeze(typeToId),
+	idToType = table.freeze(idToType)
 })
